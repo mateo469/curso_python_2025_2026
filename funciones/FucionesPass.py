ef38bc32abb1421859a7ceb17394f8da71901da9
mateo469 <montiellopezmateo@gmail.com>
2025-06-12 18:54:11 -0600

    Juegos de palabras

---
+++ funciones/FucionesPass.py
@@ -0,0 +1,26 @@
+#aprenderemos acerca de las funciones con pass
+"""la palabra clave pass se usa como un relleno temporal (placeholder) en estructuras donde se requiere una sintaxis
+válida, pero aún no se quiere escribir código. Es útil, por ejemplo, cuando estás diseñando funciones que
+planeas completar más adelante."""
+
+def mi_funcion():
+    pass
+#Esta funcion no hace nada
+"""¿Para qué se usa pass?
+Para definir la estructura de tu programa sin implementarlo aún.
+
+Para evitar errores mientras el código está en desarrollo.
+
+Como marcador de posición durante la escritura inicial del código."""
+
+def procesar_datos(datos):
+    # Aún no implementado
+    pass
+
+def mostrar_menu():
+    print("1. Opción A")
+    print("2. Opción B")
+    print("3. Salir")
+
+def salir():
+    pass  # Se implementará después
---
+++ funciones/JuegosPreguntas.py
@@ -0,0 +1,29 @@
+
+def new_game():
+    pass
+ #---------------------------------------------------------------------   
+def check_answer():
+    pass
+
+#-----------------------------------------------------------------------
+def display_score():
+    pass
+
+#-------------------------------------------------------------------------------------------
+def paly_again():
+    pass
+
+#------------------------------------------------------------------------------------------------
+preguntas = {
+    'Que idioma se habla en Brasil': 'A',
+    'Cual es el oceano mas grande del mundo': 'B',
+    'Cual es la estrella mas cerca de la tierra?': 'C',
+    'Cual es el segundo pais mas grande del mundo?': 'A'
+}
+
+#   Crear un arreglo para las opciones de respuestas
+opciones = [['A. Portugues', 'B. Español', 'C. Brasileño', 'D. Ingles'],
+            ['A. Atlantico', 'B. Pacifico', 'C. Artico', 'D. Indico'],
+            ['A. La Luna', 'B. Alfa Centauri A', 'C. El sol', 'D. Ninguna'],
+            ['A. Canada', 'B. Rusia', 'C. EE.UU', 'D. Ninguna']
+            ]
